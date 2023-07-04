ceaad82cf45bcd3340aa1443d2651d539b99966c
Fenny Oktaviani <fnny04@gmail.com>
2023-07-04 18:08:27 +0700

    improvement : add a hreff scroll

--- src/module/navbar.tsx
+++ src/module/navbar.tsx
@@ -4,30 +4,38 @@ export const Navbar: FC = (): ReactElement => {
   return (
     <div className="fixxed bottom-6 left-8 bg-white w-[350px] py-2 rounded-md">
       <div className="grid grid-cols-4 gap-2 px-4">
-        <div className="flex justify-center items-center w-20 h-20 rounded-full bg-pink-100">
-          <img
-            width="50"
-            height="50"
-            src="https://img.icons8.com/bubbles/50/confetti.png"
-            alt="confetti"
-          />
-        </div>
-        <div className="flex justify-center items-center w-20 h-20 rounded-full bg-pink-100">
-          <img
-            width="50"
-            height="50"
-            src="https://img.icons8.com/dusk/64/home--v1.png"
-            alt="home--v1"
-          />
-        </div>
-        <div className="flex justify-center items-center w-20 h-20 rounded-full bg-pink-100">
-          <img
-            width="50"
-            height="50"
-            src="https://img.icons8.com/plasticine/100/calendar--v1.png"
-            alt="calendar--v1"
-          />
-        </div>
+        <a href="#home">
+          <div className="flex justify-center items-center w-20 h-20 rounded-full bg-pink-100">
+            <img
+              width="50"
+              height="50"
+              src="https://img.icons8.com/bubbles/50/confetti.png"
+              alt="confetti"
+            />
+          </div>
+        </a>
+        <a href="#alamat">
+          {" "}
+          <div className="flex justify-center items-center w-20 h-20 rounded-full bg-pink-100">
+            <img
+              width="50"
+              height="50"
+              src="https://img.icons8.com/dusk/64/home--v1.png"
+              alt="home--v1"
+            />
+          </div>
+        </a>
+        <a href="#jadwal">
+          <div className="flex justify-center items-center w-20 h-20 rounded-full bg-pink-100">
+            <img
+              width="50"
+              height="50"
+              src="https://img.icons8.com/plasticine/100/calendar--v1.png"
+              alt="calendar--v1"
+            />
+          </div>
+        </a>
+
         <div className="flex justify-center items-center w-20 h-20 rounded-full bg-pink-100">
           <img
             width="50"
